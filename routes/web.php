bc1e1b6f8cb7313e3619314c82775920f1b84fa9
unknown <yonangetachew@gmail.com>
2020-03-18 23:34:42 +0300

    updated npm, Display questions

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/questions', 'QuestionsController');
+
+// Route::get('/q', 'QuestionsController@index')->name('q');
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
