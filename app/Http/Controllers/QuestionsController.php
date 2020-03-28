92e7e67c13f1ac4817efef5d094c0d1db9867c6a
unknown <yonangetachew@gmail.com>
2020-03-28 17:40:33 +0300

    implement delete

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -97,6 +97,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return redirect('/questions')->with('success', "Delete Successful!!");
     }
 }
