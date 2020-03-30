7bd75381c8905aea85ea522dcba9f2d3fb4b6e6f
unknown <yonangetachew@gmail.com>
2020-03-30 22:37:29 +0300

    Add vote controls to question and answer item

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,11 @@
+import fontawesome from '@fortawesome/fontawesome';
+// import {faCaretUp ,faCaretDown ,faStar ,faCheck, faCamera} from '@fortawesome/free-solid-svg-icons';
+import {faCaretUp ,faCaretDown ,faStar ,faCheck, faCamera} from '@fortawesome/fontawesome-free-solid';
+// import faCaretDown from '@fortawesome/fontawesome-free-solid';
+// import faStar from '@fortawesome/free-solid-svg-icons/faStar';
+// import faCheck from '@fortawesome/free-solid-svg-icons/faCheck';
+// import { library, icon } from '@fortawesome/fontawesome-svg-core';
+
+// library.add(faCaretDown);
+// library.add(faCamera);
+fontawesome.library.add([faCaretUp ,faCaretDown ,faStar ,faCheck]);
