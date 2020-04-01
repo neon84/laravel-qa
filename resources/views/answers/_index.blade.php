ce53c3c213471ed42001aa1f91fe08a64ca51427
unknown <yonangetachew@gmail.com>
2020-04-01 17:17:55 +0300

    Update best_answer_id on questions table when an answer removed

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -18,7 +18,7 @@
                             <a title ="This Answer is not useful" class="vote-down off"> 
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Mark answer as best answer" class="vote-accepted mt-2">
+                            <a title="Mark answer as best answer" class="{{$answer->status}} mt-2">
                                 <i class="fas fa-check fa-2x"></i>
                                 <span class="favorites-count">123</span>
                             </a>
